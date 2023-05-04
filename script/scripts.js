b10d365a59fa59d84695a383a0e17db05a61555e
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-04 03:02:01 -0300

    :lipstick: feat: Atualizando  a pagina style.css

--- script/scripts.js
+++ script/scripts.js
@@ -3,7 +3,7 @@
 const key = "455417a5d6ff4161bfce1b2a49bf66d1"
 
 function colocarDadosNaTela(dados) {
-    console.log(dados)
+    
     document.querySelector(".headline-cidade").innerHTML= "Tempo em " + dados.name
     document.querySelector(".temperatura").innerHTML= Math.floor(dados.main.temp)+"°C"
     document.querySelector(".texto-clima").innerHTML= dados.weather[0].description
