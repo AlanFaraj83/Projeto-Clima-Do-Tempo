1893e8560f7011912d761c1ef3a7dd702ba2062b
AlanFaraj83 <alanfaraj@outlook.com>
2023-05-04 02:09:58 -0300

    :sparkles:Atualizando  o script.js

--- script/scripts.js
+++ script/scripts.js
@@ -4,13 +4,16 @@ const key = "455417a5d6ff4161bfce1b2a49bf66d1"
 
 function colocarDadosNaTela(dados) {
     console.log(dados)
-    document.querySelector(".headline-cidade").innerHTML= dados.name
+    document.querySelector(".headline-cidade").innerHTML= "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML= dados.main.temp
+    document.querySelector(".texto-clima").innerHTML= dados.weather[0].description
+
 
 }
 
 
 async function buscarCidade(cidade) {
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then(response => response.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
 
     
     colocarDadosNaTela(dados)    
